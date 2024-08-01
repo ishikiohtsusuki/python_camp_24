4cc0111a30c41c0bf8922270f4308335be43108a
molofsky <molofsky@stanford.edu>
2024-07-31 23:35:32 -0700

    updates to display.py for readability

--- display.py
+++ display.py
@@ -10,6 +10,7 @@ plt.gca().get_xaxis().set_visible(False)
 plt.gca().get_yaxis().set_visible(False)
 plt.pause(3)
 plt.close()
+
 """
 
 # another solution, arguably better, to show nami image
@@ -18,18 +19,19 @@ cv2.imshow('nami', nami)
 cv2.waitKey(3000) # show for 3 seconds
 
 """
-Might want to import this file and use below function
+We might want to import this .py file and use the below function
+The import statement for this function is "from display import show_img"
 
-def show_image(img_path, time):
+def show_img(img_path, time):
     cv2.imshow('image', cv2.imread(img_path))
     cv2.waitKey(time * 1000)
 
 """
 
 """
-Could make a similar function for video 
+We might make a similar function for Luffy movie 
 
-# shows luffy movie
+# shows Luffy movie
 start = time.time()
 cap = cv2.VideoCapture("LuffyvsKatakuri.mp4")
 while (cap.isOpened()):
